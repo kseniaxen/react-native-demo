15f2c5667ef6f4d96aa53cd3a96795d3d3e73ddd
Ksenia Serdiuk <serdiuk.xen@gmail.com>
2021-01-15 00:27:11 +0200

    Todo add views and func

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Navbar } from './src/Navbar'
+import { AddTodo } from './src/AddTodo'
+import { Todo } from './src/Todo';
 
 export default function App() {
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (title) => {
+    //setTodos(todos.concat([newTodo]))
+    // setTodos((prevTodos)=>{
+    //   return [
+    //     ...prevTodos,
+    //     newTodo
+    //   ]
+    // })
+    //...prev оператор спрэд
+    setTodos(prev => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title: title
+      }
+    ])
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello!</Text>
-      <StatusBar style="auto" />
+    <View >
+      <Navbar />
+      <View style={styles.container}>
+        <AddTodo onSubmit={addTodo} />
+        <View>
+          {
+            todos.map(todo =>(<Todo todo={todo} key={todo.id}/>))
+          }
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    paddingHorizontal: 30,
+    paddingVertical: 20
+  }
 });
